c4257ae0292625e45fd38ff061a7707f6fc50b4e
ondralukes <mail@ondralukes.cz>
2020-06-17 12:49:57 +0200

    Added eslintrc (#2)

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  "env": {
+        "es6": true,
+        "browser": true,
+        "node": true,
+        "jest": true
+    },
+  "parserOptions":{
+    "ecmaVersion": 8
+  },
+  "extends": "eslint:recommended",
+  "rules" : {
+    "quotes": ["error", "single"]
+  },
+}
